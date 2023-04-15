bb23f0bd17ab5f848bf010c91d9be3b9a0c59f88
mbbill <mbbill@gmail.com>
2023-04-14 20:22:56 -0700

    removed and invalid test

--- test/unit/parser_test.c
+++ test/unit/parser_test.c
@@ -43,30 +43,30 @@ static void parser_test_full_module(void ** state) {
     module_drop(&m);
 }
 
-static void parser_test_load_file(void ** state) {
-    FILE * fp = fopen("D:\\Dev\\testwasm\\rustwasm\\target\\wasm32-wasi\\debug\\rustwasm.wasm", "rb");
-    assert_non_null(fp);
-    fseek(fp, 0, SEEK_END);
-    size_t size = ftell(fp);
-    rewind(fp);
-    u8 * wasm_bin = array_alloc(u8, size);
-    assert_non_null(wasm_bin);
-    size_t read_size = fread(wasm_bin, sizeof(u8), size, fp);
-    assert_int_equal(read_size, size);
-    fclose(fp);
+// static void parser_test_load_file(void ** state) {
+//     FILE * fp = fopen("rustwasm.wasm", "rb");
+//     assert_non_null(fp);
+//     fseek(fp, 0, SEEK_END);
+//     size_t size = ftell(fp);
+//     rewind(fp);
+//     u8 * wasm_bin = array_alloc(u8, size);
+//     assert_non_null(wasm_bin);
+//     size_t read_size = fread(wasm_bin, sizeof(u8), size, fp);
+//     assert_int_equal(read_size, size);
+//     fclose(fp);
 
-    r_vstr bin = vstr_dup(s_pl(wasm_bin, read_size));
-    assert_true(is_ok(bin));
-    array_free(wasm_bin);
-    module m = {0};
-    r ret = module_init(&m, bin.value, vs("test"));
-    assert_true(is_ok(ret));
+//     r_vstr bin = vstr_dup(s_pl(wasm_bin, read_size));
+//     assert_true(is_ok(bin));
+//     array_free(wasm_bin);
+//     module m = {0};
+//     r ret = module_init(&m, bin.value, vs("test"));
+//     assert_true(is_ok(ret));
 
-    module_drop(&m);
-}
+//     module_drop(&m);
+// }
 
 struct CMUnitTest parser_tests[] = {
-    cmocka_unit_test(parser_test_load_file),
+    // cmocka_unit_test(parser_test_load_file),
     cmocka_unit_test(parser_test_magic_mismatch),
     cmocka_unit_test(parser_test_full_module),
 };
